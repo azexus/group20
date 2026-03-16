2b775fda82b79a7eaa4089cac66fe76f9a6cf692
Besa kabwe <besakabwe11@gmail.com>
2026-03-16 09:22:55 +0200

    Added feature-square-202305153

--- api/routes.js
+++ api/routes.js
@@ -2,4 +2,9 @@ module.exports = function(app) {
   var arithmetic = require("./controllers/arithmeticController");
 
   app.route("/arithmetic").get(arithmetic.calculate);
+
+  app.get('/square/:num', function(req, res) {
+    const num = Number(req.params.num);
+    res.json({ result: num * num });
+});
 };
