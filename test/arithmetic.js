503a773afb067acff43a28ef7d76ccc80ac28dea
azexus <azexus1@proton.me>
2026-03-22 17:39:53 +0200

    repaired exponent function

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -254,11 +254,59 @@ describe('Arithmetic', function() {
     });
   });
   describe('Power', function() {
-    it('raises a number to a power', function(done) {
+    it('raises a positive integer to a positive integer power', function(done) {
       request.get('/arithmetic?operation=pow&operand1=2&operand2=3')
           .expect(200)
           .end(function(err, res) {
               expect(res.body).to.eql({ result: 8 });
+              done();
+          });
+    });
+    it('raises a positive integer to the power of zero', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=5&operand2=0')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: 1 });
+              done();
+          });
+    });
+    it('raises a positive integer to a negative power', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=2&operand2=-2')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: 0.25 });
+              done();
+          });
+    });
+    it('raises a negative integer to an even power', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=-3&operand2=2')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: 9 });
+              done();
+          });
+    });
+    it('raises a negative integer to an odd power', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=-2&operand2=3')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: -8 });
+              done();
+          });
+    });
+    it('raises zero to a positive power', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=0&operand2=5')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: 0 });
+              done();
+          });
+    });
+    it('raises a floating point number to a power', function(done) {
+      request.get('/arithmetic?operation=pow&operand1=0.5&operand2=2')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.eql({ result: 0.25 });
               done();
           });
     });
